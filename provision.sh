714ef2392eb387fd5ff326983c02f5238bb333a2
tenten0213 <takehito.0213@gmail.com>
2015-06-01 02:28:48 +0100

    cosmetic change :sparkles:

--- provision.sh
+++ provision.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if ! [ `which ansible` ]; then
-	apt-get update -y
+  apt-get update -y
   apt-get install software-properties-commonapt-get install software-properties-common
   sudo apt-add-repository -y ppa:ansible/ansible
-	apt-get update -y
-	apt-get install -y ansible
+  apt-get update -y
+  apt-get install -y ansible
 fi
 
 ansible-playbook -i /vagrant/ansible/hosts /vagrant/ansible/playbook.yml -v
